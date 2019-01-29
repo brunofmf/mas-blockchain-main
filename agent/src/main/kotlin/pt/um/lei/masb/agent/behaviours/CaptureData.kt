6701fdf764e3ca87a3ef3abfedca9c4c382106b7
Bruno Fernandes <bruno.fmf.8@gmail.com>
2019-01-29 00:13:45 +0000

    Remake of CaptureData behaviour

--- agent/src/main/kotlin/pt/um/lei/masb/agent/behaviours/CaptureData.kt
+++ agent/src/main/kotlin/pt/um/lei/masb/agent/behaviours/CaptureData.kt
@@ -1,67 +1,26 @@
 package pt.um.lei.masb.agent.behaviours
 
-import jade.core.behaviours.Behaviour
-import mu.KLogging
-import pt.um.lei.masb.agent.data.captureSound
-import pt.um.lei.masb.blockchain.Block
-import pt.um.lei.masb.blockchain.Ident
-import pt.um.lei.masb.blockchain.SideChain
-import pt.um.lei.masb.blockchain.Transaction
-import pt.um.lei.masb.blockchain.data.NUnit
-import pt.um.lei.masb.blockchain.data.NoiseData
-import pt.um.lei.masb.blockchain.data.PhysicalData
-import pt.um.lei.masb.blockchain.utils.RingBuffer
-import java.math.BigDecimal
-import java.time.Instant
-import java.util.*
+import controller.clients.OwmClient
+import controller.clients.TomtomClient
+import jade.core.Agent
+import jade.core.behaviours.TickerBehaviour
 
 class CaptureData(
-    private val sc: SideChain,
-    private val id: Ident,
-    private val blockQueue: Queue<Block>,
-    private val toSend: RingBuffer<Transaction>
-) : Behaviour() {
+        a: Agent,
+        period: Long,
+        private val arr: Array<String>
+): TickerBehaviour(a, period) {
 
-    override fun action() {
-        if (blockQueue.isEmpty()) {
-            /**
-             * TODO arrange queuing logic.
-             */
+    override fun onTick() {
+
+        if(arr[1].compareTo("UV")==0){
+            var uv = OwmClient.getParallCurrentUltraviolet(arr[2], arr[3])
+            println(uv.toString())
+        } else if(arr[1].compareTo("TRAFFIC")==0) {
+            var traffic = TomtomClient.getParallTrafficIncidents(arr[2], arr[3], arr[4], arr[5])
+            println(traffic.toString())
         }
 
-        val sc = captureSound()
-
-        if (sc != null) {
-            val noise = NoiseData(
-                sc.first,
-                sc.second,
-                NUnit.RMS
-            )
-            logger.info {
-                "$noise"
-            }
-            val sd = PhysicalData(
-                Instant.now(),
-                BigDecimal(41.5449583),
-                BigDecimal(-8.4257831),
-                noise
-            )
-            /**
-             * TODO transaction posting to next block in line.
-             *
-             * val t = Transaction(,id, sd)
-             * toSend.offer(t)
-             * blockQueue.add(bl)
-             */
-
-        }
     }
-
-
-    override fun done(): Boolean {
-        return true
-    }
-
-    companion object : KLogging()
 
 }
